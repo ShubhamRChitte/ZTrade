a9675671c7cf0eaafcfc4d6e9b0a41dc230d1130
Shubham Chitte <shubhamchitte565@gmail.com>
2025-07-31 00:12:01 +0530

    Fix cookie settings for login and signup

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -23,6 +23,8 @@ module.exports.Signup = async (req, res, next) => {
     res.cookie("token", token, {
       withCredentials: true,
       httpOnly: true,
+      secure: true,
+      sameSite: "None",
     });
 
     res.status(201).json({
@@ -56,7 +58,9 @@ module.exports.Login = async (req, res, next) => {
      const token = createSecretToken(user._id);
      res.cookie("token", token, {
        withCredentials: true,
-       httpOnly: false,
+       httpOnly: true,
+      secure: true,
+      sameSite: "None",
      });
      res.status(201).json({ message: "User logged in successfully", success: true });
      next()
